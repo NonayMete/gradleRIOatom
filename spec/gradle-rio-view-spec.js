dde86ccf8fcb452def0bcd1a6f44868edbc19b97
Jake Lorkin <jakelorkin@hotmail.com>
2017-12-14 23:31:17 +0800

    Add files via upload

---
+++ spec/gradle-rio-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GradleRioView from '../lib/gradle-rio-view';
+
+describe('GradleRioView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
